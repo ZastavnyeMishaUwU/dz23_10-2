15df39ca9e8efa3b1711d256105d22dd235eeeb3
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-10-23 21:13:01 +0300

    Add project files.

---
+++ dz23_10-2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace dz23_10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть 4 цифри по одній:");
+            int[] digits = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                digits[i] = int.Parse(Console.ReadLine());
+            }
+            string result = string.Join("", digits);
+            int number = int.Parse(result);
+
+            Console.WriteLine($"Сформоване число: {number}");
+        }
+    }
+}
